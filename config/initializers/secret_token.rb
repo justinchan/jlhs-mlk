76a028dcc7bd3b81127cac149965839815fc3743
Justin Chan <hellojustinchan@gmail.com>
2014-01-11 00:10:47 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JlhsMlk::Application.config.secret_token = '2be15d82081a7303aaf8bc6c12a573558233f662418c4bd11207bc46003d07d2a9410d6604d92529803cdffa6fb94bf1e2b49a6be9cac028fc14358f1557e15b'
